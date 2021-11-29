6f47163a27e410f5db28c9c6512bf1007fd2cf78
Rodrigo Ponce M <80773052+RODANPRO@users.noreply.github.com>
2021-11-29 20:02:55 -0300

    Add files via upload
    Tercera entrega. Android Studio + Testing

---
+++ RodrigoPonce_TrocaLibros_004V/README.md
@@ -0,0 +1,7 @@
+# RodrigoPonce_TrocaLibros_004V
+Repositorio para Proyecto Semestral Programación de Aplicaciones Móviles (004V) - App TrocaLibros
+
+Integrantes: Rodrigo Ponce
+Nombre del Proyecto: TrocaLibros
+Programación de aplicaciones móviles_004V
+
---
+++ RodrigoPonce_TrocaLibros_004V/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'info',
+    loadChildren: () => import('./pages/info/info.module').then( m => m.InfoPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'api',
+    loadChildren: () => import('./pages/api/api.module').then( m => m.ApiPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
